26e5c8cca5ed0bd0cd52e804eb4439674e14942b
Rafael Barbosa <rafaelfbarbosa7@gmail.com>
2020-06-21 22:58:33 -0300

    Task ordenation

--- src/Tasks/TaskList/TasksList.js
+++ src/Tasks/TaskList/TasksList.js
@@ -3,22 +3,33 @@ import React from 'react'
 
 import TaskElement from '../TaskElement/TaskElement'
 import classes from './TaskList.module.css'
-const taskList = (props) => (
-    <ul className={classes.TaskItems}>
-        {
-            props.tasks ?
-                props.tasks.map((task) =>
-                    <TaskElement
-                        key={task.id}
-                        task={task}
-                        handleRemove={props.handleRemove}
-                        handleCheck={props.handleCheck}
-                        handleDetails={props.handleDetails}
-                    />
-                ) :
-                <h2>No tasks to show.</h2>
-        }
-    </ul>
-)
+const taskList = (props) => {
+    let tasks = props.tasks;
+    const tasksOrderedByName =
+        tasks.sort((a, b) => {
+            return a.name < b.name ? 1 : -1;
+        })
+    const tasksOrderedByDone =
+        tasksOrderedByName.sort((a) => {
+            return a.done ? 1 : -1;
+        })
+    return (
+        <ul className={classes.TaskItems}>
+            {
+                tasks ?
+                    tasksOrderedByDone.map((task) =>
+                        <TaskElement
+                            key={task.id}
+                            task={task}
+                            handleRemove={props.handleRemove}
+                            handleCheck={props.handleCheck}
+                            handleDetails={props.handleDetails}
+                        />
+                    ) :
+                    <h2>No tasks to show.</h2>
+            }
+        </ul>
+    )
+}
 
 export default taskList
